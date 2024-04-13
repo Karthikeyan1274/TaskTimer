6f0beb7ec0e92c059c10fb88cb1901f1a930ffa6
Karthikeyan1274 <140435703+Karthikeyan1274@users.noreply.github.com>
2024-04-13 15:37:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import  {  All } from './App';
+import "./index.css";
+import "./index.html";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+
+    
+    <All />
+    </>
+
+);
+
